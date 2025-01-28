fb524900346944220da27b2e1e117f1ada6145e9
louis.molinier <louis.molinier@epita.fr>
2025-01-28 19:36:45 +0100

    Parse API Version

--- src/write.rs
+++ src/write.rs
@@ -5,13 +5,13 @@ const UNSUPPORTED_VERSION: u16 = 35;
 
 pub fn write(message: Vec<u8>, mut stream: &TcpStream) {
     let request_api_key = &message[0..2];
-    let _request_api_version = &message[2..4];
+    let request_api_version = &message[2..4];
     let correlation_id = &message[4..8];
     let _body = &message[8..];
 
     let mut data = Vec::new();
     data.extend(correlation_id);
-    if i16::from_be_bytes([request_api_key[0], request_api_key[1]]) > 4 {
+    if i16::from_be_bytes([request_api_version[0], request_api_version[1]]) > 4 {
         data.extend(UNSUPPORTED_VERSION.to_be_bytes());
     } else {
         data.extend((0 as u16).to_be_bytes());
